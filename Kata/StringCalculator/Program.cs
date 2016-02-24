96973e075317d38940e91321719c47760138d3cd
cgideon <cgideon@sourceallies.com>
2016-02-24 09:55:33 -0600

    Initial Setup

---
+++ Kata/StringCalculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
